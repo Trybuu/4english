45ebc47ddd3af29a3b11ec8572e2e8a2b868cad8
Marek Trybuła <trybu001@gmail.com>
2024-01-20 11:09:31 +0100

    Install React Router and create routes

---
+++ src/pages/EnglishLearningPage.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+function EnglishLearningPage() {
+  useEffect(() => {
+    console.log('App!')
+  }, [])
+
+  return <h1>English Learning Page</h1>
+}
+
+export default EnglishLearningPage
