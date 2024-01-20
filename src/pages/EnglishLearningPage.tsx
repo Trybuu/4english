d2e5007ad8cf29de4a00d14cfda660889150bbd1
Marek Trybuła <trybu001@gmail.com>
2024-01-20 11:20:39 +0100

    Install axios use it to make first request to random word API

--- src/pages/EnglishLearningPage.tsx
+++ src/pages/EnglishLearningPage.tsx
@@ -1,11 +1,27 @@
-import { useEffect } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 function EnglishLearningPage() {
+  const [word, setWord] = useState('Fetching word from the server...')
+
   useEffect(() => {
-    console.log('App!')
+    axios
+      .get('https://random-word-api.herokuapp.com/word')
+      .then((response) => setWord(response.data))
   }, [])
 
-  return <h1>English Learning Page</h1>
+  if (!word) {
+    return <p>Can't get word from server</p>
+  }
+
+  return (
+    <>
+      <NavLink to="..">Back to welcome page</NavLink>
+      <h1>Word for today</h1>
+      <h2>{word}</h2>
+    </>
+  )
 }
 
 export default EnglishLearningPage
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
 function HomePage() {
-  return <h1>Home Page</h1>
+  return (
+    <>
+      <h1>Home Page</h1>
+      <NavLink to="/app">Go to app</NavLink>
+    </>
+  )
 }
 
 export default HomePage
