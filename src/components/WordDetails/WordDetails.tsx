a3dacc8de115eab91f32725e711f97a3fcd82110
Marek Trybuła <trybu001@gmail.com>
2024-01-23 09:00:33 +0100

    Make Word Details component, install react-redux

---
+++ src/components/WordDetails/WordDetails.tsx
@@ -0,0 +1,9 @@
+import classes from './WordDetails.module.scss'
+
+export default function WordDetails() {
+  return (
+    <section className={classes['section-wrapper']}>
+      <h2>Word Details</h2>
+    </section>
+  )
+}
--- src/pages/EnglishLearningPage.tsx
+++ src/pages/EnglishLearningPage.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import RandomWord from '../components/RandomWord/RandomWord'
 import SearchWord from '../components/SearchWord/SearchWord'
+import WordDetails from '../components/WordDetails/WordDetails'
 
 function EnglishLearningPage() {
   return (
@@ -8,6 +9,7 @@ function EnglishLearningPage() {
       <NavLink to="..">Back to welcome page</NavLink>
       <RandomWord />
       <SearchWord />
+      <WordDetails />
     </>
   )
 }
